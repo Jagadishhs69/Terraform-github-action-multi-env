e10bbc9ec11ea19d1fa05c679a6e9976721a1ef9
Jagadishhs69 <157796587+Jagadishhs69@users.noreply.github.com>
2025-06-19 18:20:31 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "jagadishhsbucket363636"
-    key    = "envs/${var.environment}/terraform.tfstate"
+    key    = "jagadishhsbucket363636/dev/terraform.tfstate"
     region = "ap-south-1"
   }
 }
